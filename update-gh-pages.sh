72b4b907611eef37ff6b4800fc370df3971cba72
Joel Bryan Juliano <joelbryan.juliano@gmail.com>
2015-08-12 10:25:43 +0800

    Add script to prepare gh-pages

---
+++ update-gh-pages.sh
@@ -0,0 +1,13 @@
+#!/usr/bin/env bash
+
+git checkout master -- website
+
+rm -rf about.html button.html index.html docs
+
+ln website/about.html
+ln website/index.html
+mkdir docs
+ln website/docs/button.html docs/button.html
+ln website/docs/toc.html docs/toc.html
+
+git commit -m "$(date) pull from master; updated links."
