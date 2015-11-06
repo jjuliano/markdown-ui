71ba0568852a39765ee63470ebfd083a502f2512
Joel Bryan Juliano <joelbryan.juliano@gmail.com>
2015-11-06 09:54:50 +0800

    Fix bug when segment declaration is empty

--- components/elements/markdown-ui-segment/lib/segment/segment.rb
+++ components/elements/markdown-ui-segment/lib/segment/segment.rb
@@ -13,20 +13,24 @@ module MarkdownUI
     end
 
     def render
-      if @element.length == "segment".length
-        MarkdownUI::StandardSegment.new(@element, @content).render
-      elsif @mode.horizontal?
-        MarkdownUI::HorizontalSegment.new(@element, @content).render
-      elsif @mode.vertical?
-        MarkdownUI::VerticalSegment.new(@element, @content).render
-      elsif @mode.stacked?
-        MarkdownUI::StackedSegment.new(@element, @content).render
-      elsif @mode.piled?
-        MarkdownUI::PiledSegment.new(@element, @content).render
-      elsif @mode.padded?
-        MarkdownUI::PaddedSegment.new(@element, @content).render
+      if @content
+        if @element.length == "segment".length
+          MarkdownUI::StandardSegment.new(@element, @content).render
+        elsif @mode.horizontal?
+          MarkdownUI::HorizontalSegment.new(@element, @content).render
+        elsif @mode.vertical?
+          MarkdownUI::VerticalSegment.new(@element, @content).render
+        elsif @mode.stacked?
+          MarkdownUI::StackedSegment.new(@element, @content).render
+        elsif @mode.piled?
+          MarkdownUI::PiledSegment.new(@element, @content).render
+        elsif @mode.padded?
+          MarkdownUI::PaddedSegment.new(@element, @content).render
+        else
+          MarkdownUI::CustomSegment.new(@element, @content).render
+        end
       else
-        MarkdownUI::CustomSegment.new(@element, @content).render
+        MarkdownUI::CustomSegment.new(@element, "").render
       end
     end
 
