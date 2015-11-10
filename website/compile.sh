14adbb49a0e90d1b5b7ed664f3bafa57638d05fa
Joel Bryan Juliano <joelbryan.juliano@gmail.com>
2015-11-10 14:54:40 +0800

    Add Footer, Licensing and Donation info

--- website/compile.sh
+++ website/compile.sh
@@ -1,19 +1,12 @@
 #!/usr/bin/env bash
 
-for i in *.md; do
+for i in *.md docs/*.md ; do
   echo "PROCESSING FILE: $i"
+  dname="$(dirname $i)"
   name="$(basename $i .md)"
-  ../exe/markdown-ui $name.md > $name.html
+  ../exe/markdown-ui $dname/$name.md > $dname/$name.html
 
-  sed -ie 's/&#xFF1A;/:/g' $name.html 2> /dev/null
-  rm -rf $name.htmle
-done
-
-for i in docs/*.md; do
-  echo "PROCESSING FILE: $i"
-  name="$(basename $i .md)"
-  ../exe/markdown-ui $i > docs/$name.html
-
-  sed -ie 's/&#xFF1A;/:/g' docs/$name.html 2> /dev/null
-  rm -rf docs/$name.htmle
+  sed -ie 's/&#xFF1A;/:/g' $dname/$name.html 2> /dev/null
+  sed -ie 's/PAYPAL_BUTTON/https\:\/\/www\.paypal\.com\/cgi\-bin\/webscr\?cmd\=\_s\-xclick\&hosted_button_id=RVL2DAA4Y6NXL/g' $dname/$name.html 2> /dev/null
+  rm -rf $dname/$name.htmle
 done
