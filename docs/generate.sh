72fb7fa23b3f57c244535c411cd63996ed9ad523
Julian Moors <julian@Julians-MacBook-Air.local>
2020-03-31 14:40:17 +0200

    refactored docs generator

--- docs/generate.sh
+++ docs/generate.sh
@@ -4,7 +4,7 @@ for i in *.md ; do
   echo "PROCESSING FILE: $i"
   dname="$(dirname $i)"
   name="$(basename $i .md)"
-  ../exe/markdown-ui $dname/$name.md > $dname/$name.html
+  ../exe/generate-docs $dname/$name.md > $dname/$name.html
 
   sed -ie 's/&#xFF1A;/:/g' $dname/$name.html 2> /dev/null
   sed -ie 's/PAYPAL_BUTTON/https\:\/\/www\.paypal\.com\/cgi\-bin\/webscr\?cmd\=\_s\-xclick\&hosted_button_id=RVL2DAA4Y6NXL/g' $dname/$name.html 2> /dev/null
