733db8ba39b0bd77b83a8cd4ca3987b0f3e2c99e
Julian Moors <julian@Julians-MacBook-Air.local>
2020-03-31 15:06:34 +0200

    updated docs generator

--- docs/generate.sh
+++ docs/generate.sh
@@ -4,7 +4,11 @@ for i in *.md ; do
   echo "PROCESSING FILE: $i"
   dname="$(dirname $i)"
   name="$(basename $i .md)"
-  ../exe/generate-docs $dname/$name.md > $dname/$name.html
+  if [ "$1" = "preview" ]; then
+    ../exe/markdown-ui $dname/$name.md > $dname/$name.html
+  else
+    ../exe/generate-docs $dname/$name.md > $dname/$name.html
+  fi
 
   sed -ie 's/&#xFF1A;/:/g' $dname/$name.html 2> /dev/null
   sed -ie 's/PAYPAL_BUTTON/https\:\/\/www\.paypal\.com\/cgi\-bin\/webscr\?cmd\=\_s\-xclick\&hosted_button_id=RVL2DAA4Y6NXL/g' $dname/$name.html 2> /dev/null
